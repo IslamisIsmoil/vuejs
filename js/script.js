41d27e3e5645b8669005c29338418ea521749f27
IslamisIsmoil <islamirgashev7@gmail.com>
2020-10-13 11:40:07 +0500

    make it better

--- js/script.js
+++ js/script.js
@@ -26,6 +26,20 @@ var app4 = new Vue ({
             { text: 'Learn JavaScript' },
             { text: 'Learn Vue' },
             { text: 'Learn Laravel' }
+            // In the console, enter app4.todos.push({ text: 'New item' }).
+            // You should see a new item appended to the list.
         ]
+    }
+});
+
+var app5 = new Vue ({
+    el: "#app-5",
+    data: {
+        message: "Hello Vue.js!"
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join(''); 
+        }
     }
 });
